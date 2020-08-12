318247f33353cd5b50db18477f4b21776054e2da
Alex Uribe <alexdvduc@gmail.com>
2020-08-12 17:28:05 -0500

    Calculadora con validadores de ingreso

--- Calculadora.py
+++ Calculadora.py
@@ -1,35 +1,65 @@
+print()
+print()
 print('----------------------------------------')
 print('----------     CALCULADORA    ----------')
 print('----------------------------------------')
-print('----------------------------------------')
-print('----------------------------------------')
-print('  "Para realizar la operacion deseada, \n sigue las instrucciones:"')
-print('----------------------------------------')
-print('----------------------------------------')
-print('Si deseas sumar ingresa el signo +')
-print('Si deseas restar ingresa el signo -')
-print('Si deseas multiplicarar ingresa el signo *')
-print('Si deseas dividir ingresa el signo /')
-print('----------------------------------------')
-print('----------------------------------------')
-print('----------------------------------------')
+print()
+print()
+print('  "Para realizar la operacion deseada, \n      sigue las instrucciones: -->"')
+print()
+print()
+print(' *** Si deseas sumar ingresa el signo -------->    + ')
+print(' *** Si deseas restar ingresa el signo ------->    - ')
+print(' *** Si deseas multiplicar ingresa el signo -->    * ')
+print(' *** Si deseas dividir ingresa el signo ------>    / ')
+print()
+print()
+print()
+
+operadores = ['+', '-', '*', '/']
+valores = float
 
 operacion = input('Ingresa el simbolo de la operacion a realizar: \n')
-valor1 = int(input('Ingresa el primer valor de la operación: \n'))
-valor2 = int(input('Ingresa el primer valor de la operación: \n'))
+
+while operacion not in operadores:
+    print('Error -> No es una operación reconocida, trata de nuevo')
+    operacion = input('Ingresa el simbolo de la operacion a realizar: \n')
+
+while True:
+    try:
+        valor1 = input('Ingresa el primer valor de la operación: \n')
+        valor1 = float(valor1)
+
+    except ValueError:
+        print('debiste poner un número')
+
+    else:
+        break
+
+while True:
+    try:
+        valor2 = input('Ingresa el segundo valor de la operación: \n')
+        valor2 = float(valor2)
+
+    except ValueError:
+        print('debiste poner un número')
+
+    else:
+        break
 
 
 if operacion == '+':
     mas = valor1 + valor2
-    print('El resultado de la suma es {}'.format(mas))
+    print('El resultado de la suma de {} más {} es {}'.format(valor1, valor2, mas))
+
 elif operacion == '-':
     menos = valor1 - valor2
-    print('El resultado de la resta es {}'.format(menos))
+    print('El resultado de la resta {} menos {} es {}'.format(valor1, valor2, menos))
+
 elif operacion == '*':
     multiplicacion = valor1 * valor2
-    print('El resultado de la multiplicación es {}'.format(multiplicacion))
+    print('El resultado de la multiplicación de {} por {} es {}'.format(valor1, valor2, multiplicacion))
+
 elif operacion == '/':
     division = valor1 / valor2
-    print('El resultado de la división es {}'.format(division))
-else:
-    print('No es una operacin reconocida')
+    print('El resultado de la división de {} entre {} es {}'.format(valor1, valor2, division))
